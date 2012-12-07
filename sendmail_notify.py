e99ff088273d1ae50e9dbb267b1f318f0474d123
Trey Morris <treyemorris@gmail.com>
2012-12-07 17:16:40 -0600

    updated comments, adding GPL3 license and options

--- sendmail_notify.py
+++ sendmail_notify.py
@@ -1,9 +1,45 @@
 """
-Author: Trey Morris <treyemorris@gmail.com>
-Homepage: https://github.com/tr3buchet/weechat_sendmail_notify.py/
+Author: Trey Morris <trey@treymorris.com>
+Homepage: http://github.com/tr3buchet/weechat_plugins
 Version: 1.0
 
+License
+------------
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+Description
+------------
 This plugin is for using sendmail to send notifications
+
+
+Options
+------------
+    plugins.var.python.sendmail_notify.sendmail_location
+
+        The sendmail executable.
+        Default: /usr/sbin/sendmail
+
+    plugins.var.python.sendmail_notify.from
+
+        The email address the message will be from.
+        Default: ''
+
+    plugins.var.python.sendmail_notify.to
+
+        The email addres the message will be sent to.
+        Default: ''
 """
 
 import weechat
@@ -12,7 +48,7 @@ from subprocess import Popen, PIPE
 
 # register script
 weechat.register('sendmail_notify', 'Trey Morris', '1.0',
-                 'Some License',
+                 'GPL3',
                  'sendmail_notify: send notifications using sendmail',
                  '', '')
 
@@ -46,7 +82,9 @@ weechat.hook_config("plugins.var.python.sendmail_notify.*",
 
 
 def send_message(data, signal, signal_data):
-    """Callback called when highlight or private message is received"""
+    """Callback called when highlight or private message is received.
+       Creates an email and uses subprocess to call sendmail to send it.
+    """
     msg = MIMEText(signal_data)
     msg['From'] = config['from']
     msg['To'] = config['to']
@@ -58,7 +96,10 @@ def send_message(data, signal, signal_data):
 
 
 def config_callback(data, option, value):
-    """Callback called when a script option is changed."""
+    """Callback called when a script option is changed.
+       Stores the config value so it can be retrieved locally when sending
+       messages. (may be an unnecessary optimization)
+    """
     option = option.split('.')[-1]
-    weechat.prnt('', 'config callback |%s|%s|%s|' % (data, option, value))
+    config[option] = value
     return weechat.WEECHAT_RC_OK
